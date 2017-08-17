16d29b58a0a3d486f05474bbdf7a17c87dc8f347
thushanp <tpuhalendran@college.harvard.edu>
2017-08-16 20:48:33 -0400

    alternating binary init

---
+++ alternatingbinary.py
@@ -0,0 +1,3 @@
+# Given a long list comprising of binaries digits, 0/1 in random order, 
+# achieve the longest possible alternating sequence without using extra space.
+# I gave a solution in quadratic time then optimized to linear.
