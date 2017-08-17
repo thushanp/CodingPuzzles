1aafdf9dbd79a9608e78de00b6a90cd07af05024
thushanp <tpuhalendran@college.harvard.edu>
2017-08-16 22:08:21 -0400

    alternating binary

--- alternatingbinary.py
+++ alternatingbinary.py
@@ -1,3 +1,23 @@
 # Given a long list comprising of binaries digits, 0/1 in random order, 
-# achieve the longest possible alternating sequence without using extra space.
-# I gave a solution in quadratic time then optimized to linear.
+# build the longest possible alternating sequence without using extra space.
+
+# quadratic O(n^2) solution
+
+random = [0,1,0,1,1,1,0,0,0,1,1,1,1,0,0]
+
+print(random)
+
+for i in range(0,len(random)-1):
+	if random[i] == 0:
+		for x in range(i,len(random)):
+			if random[x] == 1:
+				del random[x]
+				random.insert(i+1,1)
+	elif random[i] == 1:
+		for x in range(i,len(random)):
+			if random[x] == 0:
+				del random[x]
+				random.insert(i+1,0)
+
+print(random)
+
