beb77fbf32c6ad8c75edb3f259f7998f2fa185a0
thushanp <tpuhalendran@college.harvard.edu>
2017-08-18 08:49:57 -0400

    findalternating almost done

--- findalternatingsubseq.py
+++ findalternatingsubseq.py
@@ -1,31 +1,31 @@
 # Given a long list comprising of binaries digits, 0/1 in random order, 
 # find the longest alternating subsequence without using extra space.
 
-# I gave a solution in quadratic time then optimized to linear.
+# O(n) solution here:
 
-random = [0,1,0,1,1,1,0,0,0,1,1,1,1,0,0]
+second = [1,0,1,0,0,0,0,0,1,1,0,1,0,1,0,0,0,0]
 
-print(random)
-
-# O(n)
-
-tracker = 0
-currentbeg = 0
-currenthigh = 0
 
 def findalternator(array):
+	tracker = 0
+	currentbeg = 0
+	currenthigh = 0
 	for i in range(0,len(array)):
-		if array[i] == 0 and array[i+1] == 1:
-			tracker += 1
-			if tracker != 1:
-				beginning = array[i]
-		elif array[i] == 1 and array[i+1] == 0:
-			tracker += 1
-			if tracker != 1:
-				beginning = array[i]
-		elif tracker > currenthigh:
-			currenthigh = tracker
-			currentbeg = beginning
-			tracker = 0
+		if i != len(array) - 1:
+			if array[i] == 0 and array[i+1] == 1:
+				tracker += 1
+				if tracker == 1:
+					beginning = array[i]
+			elif array[i] == 1 and array[i+1] == 0:
+				tracker += 1
+				if tracker == 1:
+					beginning = array[i]
+			elif tracker > currenthigh:
+				currenthigh = tracker + 1
+				currentbeg = beginning
+				tracker = 0
+			else:
+				tracker = 0
+	return currentbeg, currenthigh
 
-print(currentbeg, currenthigh)
+print(findalternator(second))
